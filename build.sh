7e08e73a2ea674c789788bb16b46db1b1dc9e0b3
CHEN Xianren <chenxianren@gmail.com>
2023-06-02 18:12:33 +0800

    feature: arm64

--- build.sh
+++ build.sh
@@ -16,14 +16,25 @@ image="${DOCKER_REGISTRY}/erda-fluent-bit:${v}"
 
 echo "image=${image}"
 
-DOCKER_BUILDKIT=1 docker build -t "${image}" \
-    --label "branch=$(git rev-parse --abbrev-ref HEAD)" \
-    --label "commit=$(git rev-parse HEAD)" \
-    --label "build-time=$(date '+%Y-%m-%d %T%z')" \
-    -f dockerfiles/Dockerfile .
-
 docker login -u "${DOCKER_REGISTRY_USERNAME}" -p "${DOCKER_REGISTRY_PASSWORD}" "${DOCKER_REGISTRY}"
+#docker buildx create --use
+#docker buildx build --platform linux/amd64,linux/arm64 -t "${image}" \
+#docker buildx build --platform linux/${ARCH} -t "${image}" \
+#    --label "branch=$(git rev-parse --abbrev-ref HEAD)" \
+#    --label "commit=$(git rev-parse HEAD)" \
+#    --label "build-time=$(date '+%Y-%m-%d %T%z')" \
+#    --push \
+#    -f dockerfiles/Dockerfile .
 
-docker push "${image}"
+buildctl --addr tcp://buildkitd.default.svc.cluster.local:1234 \
+    --tlscacert=/.buildkit/ca.pem \
+    --tlscert=/.buildkit/cert.pem \
+    --tlskey=/.buildkit/key.pem \
+    build \
+    --frontend dockerfile.v0 \
+    --opt platform=${PLATFORMS} \
+    --local context=/.pipeline/container/context/fluent-bit \
+    --local dockerfile=/.pipeline/container/context/fluent-bit/dockerfiles \
+    --output type=image,name=${image},push=true
 
 echo "image=${image}" >> $METAFILE
